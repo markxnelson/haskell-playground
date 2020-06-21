5b8f9a26ea12b1aac6f51b3bbc7c9eb7919db0c5
Mark Nelson <mark.x.nelson@oracle.com>
2020-06-20 22:04:20 -0400

    declarative gtk

--- gtk-intro/app/Main.hs
+++ gtk-intro/app/Main.hs
@@ -1,29 +1,64 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
-import Data.GI.Base
+import Control.Monad (void)
+import Data.Text (Text)
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
 import qualified GI.Gtk as Gtk
+import GI.Gtk.Declarative
+import GI.Gtk.Declarative.App.Simple
+
+data Event
+    = TodoTextChanged Text
+    | TodoSubmitted
+    | Closed
+
+data Todo = Todo
+    { name :: Text
+    }
+
+data State = State
+    { todos :: Vector Todo
+    , currentText :: Text
+    }
 
 main :: IO ()
-main = do
-    Gtk.init Nothing
+main = void $ run App
+    { view = view'
+    , update = update'
+    , inputs = []
+    , initialState = State {todos = mempty, currentText = mempty}
+    }
 
-    win <- new Gtk.Window [#title := "Introduction"]
-    on win #destroy Gtk.mainQuit
-    #resize win 640 480
+view' :: State -> AppView Gtk.Window Event
+view' s = bin
+    Gtk.Window
+    [ #title := "TodoGtk+"
+    , on #deleteEvent (const (True, Closed))
+    ]
+    (container Gtk.Box
+        [#orientation := Gtk.OrientationVertical]
+        [todoList, newTodoForm]
+    )
+    where
+        todoList = container Gtk.Box
+            [#orientation := Gtk.OrientationVertical]
+            (fmap todoItem (todos s))
+        todoItem todo = widget Gtk.Label [#label := name todo]
+        newTodoForm = widget
+            Gtk.Entry
+            [ #text := currentText s
+            , #placeholderText := "What needs to be done?"
+            , onM #changed (fmap TodoTextChanged . Gtk.entryGetText)
+            , on #activate TodoSubmitted
+            ]
 
-    box <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
-    #add win box
-
-    msg <- new Gtk.Label [#label := "Hello"]
-    #packStart box msg True False 10
-
-    btn <- new Gtk.Button [#label := "Click me!"]
-    #packStart box btn True False 10
-    on btn #clicked (set msg [#label := "Clicked!"])
-
-    #showAll win
-
-    Gtk.main
+update' :: State -> Event -> Transition State Event
+update' s e = case e of
+    TodoTextChanged t -> Transition s {currentText = t} (pure Nothing)
+    Closed -> Exit
