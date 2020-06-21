5ed7ab28aeb0fa261ee8b4c0ffb62543215e39e2
Mark Nelson <mark.x.nelson@oracle.com>
2020-06-20 21:30:15 -0400

    initial gtk intro app

---
+++ gtk-intro/README.md
@@ -0,0 +1,15 @@
+# gtk-intro
+
+
+## prereqs
+
+* Install OS libraries;
+
+    ```
+    apt install \
+        gobject-introspection \
+        libgirepository1.0-dev \
+        libwebkit2gtk-4.0-dev \
+        libgtksourceview-3.0-dev \
+        appmenu-gtk3-module
+    ```
---
+++ gtk-intro/app/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Lib
+import Data.GI.Base
+import qualified GI.Gtk as Gtk
+
+main :: IO ()
+main = do
+    Gtk.init Nothing
+
+    win <- new Gtk.Window [#title := "Introduction"]
+    on win #destroy Gtk.mainQuit
+    #resize win 640 480
+
+    box <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
+    #add win box
+
+    msg <- new Gtk.Label [#label := "Hello"]
+    #packStart box msg True False 10
+
+    btn <- new Gtk.Button [#label := "Click me!"]
+    #packStart box btn True False 10
+    on btn #clicked (set msg [#label := "Clicked!"])
+
+    #showAll win
+
+    Gtk.main
